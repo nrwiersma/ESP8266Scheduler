c8ff6288e9a466377ae3014582efe52fdbb680b1
Nicholas Wiersma <nick@wiersma.co.za>
2021-02-06 21:51:42 +0200

    chore: fix formatting

--- src/Task.h
+++ src/Task.h
@@ -5,83 +5,80 @@
 //#include "Scheduler.h"
 
 extern "C" {
-    #include "cont.h"
+#include "cont.h"
 }
 
 class Task {
-public:
-    Task() {
-        cont_init(&context);
+ public:
+  Task() { cont_init(&context); }
+
+ protected:
+  virtual void setup() {}
+
+  virtual void loop() {}
+
+  void delay(unsigned long ms) {
+    if (ms) {
+      delay_start = millis();
+      delay_ms = ms;
     }
 
-protected:
-    virtual void setup() {}
+    yield();
+  }
 
-    virtual void loop() {}
+  void yield() { cont_yield(&context); }
 
-    void delay(unsigned long ms) {
-        if (ms) {
-            delay_start = millis();
-            delay_ms = ms;
-        }
+  inline bool isDelayed() { return (delay_ms != 0); }
 
-        yield();
+  void updateDelayTimer() {
+    if (delay_ms == 0)
+      return;  // Optimize for the non-delayed case
+
+    // This comparison is "rollover safe"
+    unsigned long now = millis();
+    if ((now - delay_start) >= delay_ms)
+      delay_ms = 0;
+  }
+
+  virtual bool shouldRun() {
+    // Tasks update their own delay timer
+    updateDelayTimer();
+    if (isDelayed())
+      return false;
+    if (!run_group_active)
+      return false;
+    return !loop_complete;
+  }
+
+  uint8_t current_cycle_id = 0;
+  uint8_t run_group_id = 0xFF;
+  bool run_group_active = false;
+
+  bool loop_complete = false;
+
+ private:
+  friend class SchedulerClass;
+  friend void task_tramponline();
+
+  Task* next;
+  Task* prev;
+  cont_t context;
+
+  bool setup_done = false;
+  unsigned long delay_start = 0;
+  unsigned long delay_ms = 0;
+
+  void loopWrapper() {
+    if (!setup_done) {
+      setup();
+      setup_done = true;
     }
 
-    void yield() {
-        cont_yield(&context);
+    while (1) {
+      loop();
+      yield();
     }
-
-	inline bool isDelayed() {
-		return (delay_ms != 0);
-	}
-
-	void updateDelayTimer() {
-		if (delay_ms == 0) return;   // Optimize for the non-delayed case
-
-		// This comparison is "rollover safe"
-        unsigned long now = millis();
-        if ((now - delay_start) >= delay_ms)
-			delay_ms = 0;
-	}
-
-    virtual bool shouldRun() {
-		// Tasks update their own delay timer
-		updateDelayTimer();
-		if (isDelayed()) return false;
-        if (!run_group_active) return false;
-		return !loop_complete;
-	}
-
-    uint8_t current_cycle_id = 0;
-    uint8_t run_group_id = 0xFF;
-    bool run_group_active = false;
-	
-    bool loop_complete = false;
-private:
-    friend class SchedulerClass;
-    friend void task_tramponline();
-
-    Task *next;
-    Task *prev;
-    cont_t context;
-
-
-    bool setup_done = false;
-    unsigned long delay_start = 0;
-    unsigned long delay_ms = 0;
-
-    void loopWrapper() {
-        if (!setup_done) {
-            setup();
-            setup_done = true;
-        }
-
-        while(1) {
-            loop();
-            yield();
-        }
-    }
+  }
 };
 
 #endif
